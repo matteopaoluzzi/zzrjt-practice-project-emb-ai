2103aaa1697300a9e895071d2b2999e9cf67fb79
PAOLUZZIM <matteo.paoluzzi@leonardo.com>
2026-09-23 11:54:51 +0200

    task 8 static code analysis

--- server.py
+++ server.py
@@ -33,11 +33,8 @@ def sent_analyzer():
     # Check if the label is None, indicating an error or invalid input
     if label is None:
         return "Invalid input! Try again."
-    else:
-        # Return a formatted string with the sentiment label and score
-        return "The given text has been identified as {} with a score of {}.".format(
-            label.split("_")[1], score
-        )
+    # Return a formatted string with the sentiment label and score
+    return f"The given text has been identified as {label.split('_')[1]} with a score of {score}."
 
 
 @app.route("/")
@@ -49,5 +46,4 @@ def render_index_page():
 
 
 if __name__ == "__main__":
-    """This functions executes the flask app and deploys it on localhost:5000"""
     app.run(host="0.0.0.0", port=5000)
